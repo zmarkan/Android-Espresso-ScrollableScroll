e86b7969912d2280132f1cc94b5d63bc7fde36ca
zmarkan <zan@circleci.com>
2021-09-24 10:25:30 +0100

    avoid running regular build on schedule, improve deletion script

--- build-scheduling/delete_schedule.js
+++ build-scheduling/delete_schedule.js
@@ -7,18 +7,18 @@ const org = process.env.ORG_NAME
 const project = process.env.PROJECT_ID
 const token = process.env.CIRCLECI_TOKEN
 
-const schedule_id = 'YOUR_SCHEDULE_ID'
-const deleteScheduleEndpoint = `${API_BASE_URL}/${schedule_id}`
+const schedule_ids  = [
+    'ID_TO_DELETE',
+]
 
 async function deleteScheduleById(){
-
-    let res = await axios.delete(deleteScheduleEndpoint,
-        {
+    for(let i = 0; i< schedule_ids.length; i++){
+        let deleteScheduleEndpoint = `${API_BASE_URL}/${schedule_ids[i]}`
+        let res = await axios.delete(deleteScheduleEndpoint, {
             headers: { 'circle-token': token }
-        }
-    )
-
-    console.log(res.data)
+        })
+        console.log(res.data)
+    }
 }
 
 deleteScheduleById()
